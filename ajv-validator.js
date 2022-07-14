4d8187e110c433347852b888a27af953f2056de3
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2022-07-14 15:08:39 +0600

    AJV Validator - JSON Schema

---
+++ ajv-validator.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
+
+const ajv = new Ajv({ allErrors: true });
+addFormats(ajv);
+
+const app = express();
+app.use([morgan('dev'), cors(), express.json()]);
+
+// handle registration
+app.post('/', (req, res) => {
+  const validate = ajv.compile(require('./registration.schema.json'));
+  const valid = validate(req.body);
+
+  if (!valid) {
+    console.log(validate.errors);
+    return res.status(400).json(validate.errors);
+  }
+  res.status(203).json({ message: 'OK' });
+});
+
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000');
+});
+
+// Demo user
+const reqBody = {
+  name: 'MD Sakil', // required
+  email: 'sakil@gmail.com', // required
+  password: 'pass123', // required
+  confirmPassword: 'pass123', // required
+  bio: 'Backend Developer',
+  addresses: [
+    {
+      city: 'Bhanga',
+      postCode: 7830,
+    },
+    {
+      city: 'Faridpur',
+      postCode: 7800,
+    },
+  ],
+  skills: 'Javascript, Typescript, React, Node',
+};
